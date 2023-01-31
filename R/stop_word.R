e2cfa6c793bb882f2a63b59bf57bc41e4c71c7b7
Ryan Schaefer <rschaefer1@me.com>
2023-01-30 20:06:56 -0600

    Add stop_word data

---
+++ R/stop_word.R
@@ -0,0 +1,20 @@
+#' Stop Words List
+#'
+#' A list of common words (known as stop words) that can be used to remove insignificant words from your data.
+#'
+#' @docType data
+#'
+#' @usage stop_word
+#'
+#' @format A character vector with 478 values
+#' 
+#' @keywords datasets
+#'
+#' @examples
+#' data(stop_word)
+#'
+#' @references Buongiorno, Steph (2021) 
+#'
+#' @source {../data/stop_word.RData}
+#'
+"stop_word"
